46ec2aeb4ce637f1593ef95236d0ed9bf526db0f
1llen <allenheldhostage@gmail.com>
2024-04-17 03:06:34 -0400

    Added requriements and run_script

---
+++ run_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change directory to the Python-Web-Scraper folder
+cd /etc/Python-Web-Scraper
+
+# Install python dependencies
+pip install -r requirements.txt
+
+# Run python script
+python main.py
